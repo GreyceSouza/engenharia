eb0620e3c156352eab28b702cd7bf45997db4e71
GreyceSouza <greyce.c.s.s@hotmail.com>
2017-11-10 22:05:46 -0200

    Add files via upload

---
+++ Tia31440721/Tia31440721/src/Calculos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 31440721
+ */
+public class Calculos {
+    
+    public float calc(int[] digitos, int modo) {
+        return 0;
+    }
+    
+    
+    
+}
---
+++ Tia31440721/Tia31440721/test/CalculosTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31440721
+ */
+public class CalculosTest {
+    
+    public CalculosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calc method, of class Calculos.
+     */
+    @Test
+    public void testCalc() {
+        System.out.println("calc");
+        int[] digitos = null;
+        int modo = 0;
+        Calculos instance = new Calculos();
+        float expResult = 0.0F;
+        float result = instance.calc(digitos, modo);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
